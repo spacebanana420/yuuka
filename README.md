2a43d7ee3fee25fd6b1026bfee76af2f294c5008
Space Banana <tomasbessa@protonmail.com>
2024-09-13 20:24:38 +0100

    Help message and readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# yuuka
-Simple and lightweight build tool for Java projects
+# Yuuka
+Yuuka is a lightweight build tool for compiling your Java projects in a quick and easy way. This is a brand new repository.
--- src/yuuka/main.java
+++ src/yuuka/main.java
@@ -4,4 +4,20 @@ public class main {
   public static void main(String[] args) {
     
   }
+
+  private static String getHelpMessage() {
+    return
+      "Yuuka help screen"
+      + "\nBasic usage: yuuka [command] [options]"
+      + "\n\nAvailable commands:"
+      + "\n  * init - creates a project file structure"
+      + "\n  * build - compiles your project"
+      + "\n  * package - compiles your project and packages it into an executable JAR"
+      + "\n  * run - compiles and runs your project"
+      + "\n  * clean - deletes all .class files"
+      
+      + "\n\nAvailable CLI arguments:"
+      + "\n--release [number] - sets the target Java release for your software"
+      + "\n--ingore-lib - ignores all library JARs that are in lib";
+  }
 }
