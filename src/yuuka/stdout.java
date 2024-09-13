d67326d00e869c3556c564db1bb400b5cce54663
Space Banana <tomasbessa@protonmail.com>
2024-09-14 00:06:32 +0100

    Printing functions

---
+++ src/yuuka/stdout.java
@@ -0,0 +1,27 @@
+package yuuka;
+
+public class stdout {
+  public static void print(String message) {
+    if (!globalvariables.SILENT) {System.out.println(message);}
+  }
+
+  public static void print(String title, String[] contents) {
+    if (!globalvariables.SILENT) {printSeq(title, contents);}
+  }
+
+  public static void print_verbose(String message) {
+    if (globalvariables.VERBOSE && !globalvariables.SILENT) {System.out.println(message);}
+  }
+
+  public static void print_verbose(String title, String[] contents) {
+    if (globalvariables.VERBOSE && !globalvariables.SILENT) {printSeq(title, contents);}
+  }
+
+  private static void printSeq(String title, String[] contents) {
+    String txt = title;
+    for (String c : contents) {
+      txt += "\n" + c;
+    }
+    System.out.println(txt);
+  }
+}
