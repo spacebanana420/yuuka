653e1c1686bc74ed24c96da0038c8eb734a8a910
Space Banana <tomasbessa@protonmail.com>
2024-10-19 23:58:48 +0100

    Process information prints only in debug mode

--- src/yuuka/process.java
+++ src/yuuka/process.java
@@ -9,7 +9,7 @@ public class process {
   //todo: add output directory
   public static int runProcess(String[] cmd, String working_directory) {
     try {
-      stdout.print_verbose("Executing the process:\n", cmd);
+      stdout.print_debug("Executing the process:\n", cmd);
       var process =
         new ProcessBuilder(cmd)
         .inheritIO()
