3345597fbe86cdcd6f1ad04b878049822d24aa3f
Space Banana <tomasbessa@protonmail.com>
2025-07-24 18:57:54 +0100

    Version change

--- src/yuuka/cli/help.java
+++ src/yuuka/cli/help.java
@@ -2,7 +2,7 @@ package yuuka.cli;
 
 public class help {
   
-    static String title() {return "Yuuka version 1.3.2";}
+    static String title() {return "Yuuka version 1.3.3";}
   
     public static String getHelpMessage() {
     return
