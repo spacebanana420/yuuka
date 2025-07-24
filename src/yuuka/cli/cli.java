38673c1ac149add1b359bc3fb4454c9b0fa5f407
Space Banana <tomasbessa@protonmail.com>
2025-07-24 18:40:02 +0100

    More CLI parsing error messages

--- src/yuuka/cli/cli.java
+++ src/yuuka/cli/cli.java
@@ -4,8 +4,7 @@ import java.io.File;
 import java.util.ArrayList;
 
 import yuuka.global;
-import yuuka.io.stdout;
-import yuuka.io.fileops;
+import yuuka.io.*;
 import yuuka.misc;
 import yuuka.jdk.tests;
 import yuuka.jdk.installer;
@@ -93,28 +92,39 @@ public class cli {
   
   static void assignValue_main(String[] args, int parse_break) {
     int i = parser.findArgument(args, parse_break, "-m", "--main");
-    if (i != -1 && parser.hasArgumentValue(args, i)) {
-      global.MAIN_CLASS = args[i+1];
-    }
+    if (i == -1) {return;}
+    
+    String err = args[i] + " must be followed by the path to the project's main class!\nFor example yuuka/main";
+    if (checkForValue(i, args, err)) {global.MAIN_CLASS = args[i+1];}
   }
   static void assignValue_output(String[] args, int parse_break) {
     int i = parser.findArgument(args, parse_break, "-o", "--output");
-    if (i != -1 && parser.hasArgumentValue(args, i)) {
-      global.setProgramName(args[i+1]);
-    }
+    if (i == -1) {return;}
+    
+    String err = args[i] + " must be followed by the name of the JAR file!\nFor example yuuka.jar";
+    if (checkForValue(i, args, err)) {global.setProgramName(args[i+1]);}
   }
+  
   static void assignValue_graalvm(String[] args, int parse_break) {
     int i = parser.findArgument(args, parse_break, "-gp", "--graal-path");
-    if (i != -1 && parser.hasArgumentValue(args, i)) {
-      var binaryf = new File(args[i+1]);
+    if (i == -1) {return;}
+    
+    String err = args[i] + " must be followed by the path to the GraalVM's native-image binary!\nFor example /usr/local/bin/native-image";
+    if (checkForValue(i, args, err)) {
+      File binaryf = new File(args[i+1]);
       if (binaryf.isFile() && binaryf.canExecute()) {global.GRAAL_PATH = args[i+1];}
+      else {stdout.error("The GraalVM native-image binary at path " + args[i+1] + " does not exist or is not executable!");}
     }
   }
   static void assignValue_install(String[] args, int parse_break) {
     int i = parser.findArgument(args, parse_break, "-ip", "--install-path");
-    if (i != -1 && parser.hasArgumentValue(args, i)) {
-      var installf = new File(args[i+1]);
+    if (i == -1) {return;}
+    
+    String err = args[i] + " must be followed by the installation path!\nFor example /usr/local/bin";
+    if (checkForValue(i, args, err)) {
+      File installf = new File(args[i+1]);
       if (installf.isDirectory() && installf.canWrite()) {global.INSTALL_PATH = args[i+1];}
+      else {stdout.error("The installation path " + args[i+1] + " does not exist or cannot be written to!");}
     }
   }
   
@@ -135,6 +145,7 @@ public class cli {
     return null;
   }
   
+  //The argument -- defines the end of Yuuka CLI arguments and the start of CLI arguments to pass to the programs or tests you run
   public static int findParseBreak(String[] args) {
     for (int i = 0; i < args.length; i++) {if (args[i].equals("--")) {return i;}}
     return args.length;
@@ -151,5 +162,10 @@ public class cli {
     
     for (int i = args_start; i < args.length; i++) {exec_args.add(args[i]);}
     return exec_args.toArray(new String[0]);
+  }
+  
+  private static boolean checkForValue(int i, String[] args, String error_message) {
+    if (!parser.hasArgumentValue(args, i)) {stdout.error(error_message); return false;}
+    return true;
   }
 }
